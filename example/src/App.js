8dccf32e8defe03c45b88a0bd6e0b281e1d31e20
Jorge Corradi <jlcorradi@gmail.com>
2020-04-05 14:49:09 -0300

    init create-react-library@3.0.6

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'jlc-react-utils'
+import 'jlc-react-utils/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
